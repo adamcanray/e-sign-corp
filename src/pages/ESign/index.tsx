2459bb89b4c6264771b40a31a814dc87306f7be6
adamcanray <adamcool180@gmail.com>
2021-12-29 10:02:21 +0700

    add some className

--- src/pages/ESign/index.tsx
+++ src/pages/ESign/index.tsx
@@ -121,10 +121,16 @@ const ESign = () => {
               }}
             />
             <div>
-              <button className="bg-gray-300 px-4 py-2" onClick={clear}>
+              <button
+                className="bg-gray-300 hover:bg-gray-400 px-4 py-2"
+                onClick={clear}
+              >
                 Clear
               </button>
-              <button className="bg-gray-300 px-4 py-2" onClick={trim}>
+              <button
+                className="bg-gray-300 hover:bg-gray-400 px-4 py-2"
+                onClick={trim}
+              >
                 Trim
               </button>
             </div>
